cf8a56ed74de02a9700391f43744c0a46f7107a0
honeydev783 <adamsaidev783@outlook.com>
2025-01-20 05:01:24 +0800

    jquery lib

--- resources/views/estimate_form/cost-form.blade.php
+++ resources/views/estimate_form/cost-form.blade.php
@@ -4,3 +4,7 @@
 <input type="hidden" name="id" value="{{$data['id']}}" id="approx_id">
 <cost-estimate-form></cost-estimate-form>
 @endsection
+@section('scripts')
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+@endsection
